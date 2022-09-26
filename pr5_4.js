fbaf3bd7b007af0a36a945276ea0d3836bf9e62f
nirnay01 <ravalnirnay@gmail.com>
2022-09-26 10:27:25 +0530

    hi

---
+++ pr5_4.js
@@ -0,0 +1,8 @@
+// Do as directed:
+// a. Remove a specific listener.
+// b. Remove all listeners.
+// c. How to get the maximum size of listener that will bind with a specific event
+// d. How to change the maximum size of listeners to bind with specific events
+// e. Find out the name of active listeners for the specified event.
+// f. Find out the number of listeners listening to the specified event.
+// g. How to change the execution position of listeners
