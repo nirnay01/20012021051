6044da16350e31d428129299950d91cb8c21e277
nirnay01 <ravalnirnay@gmail.com>
2022-09-26 09:39:13 +0530

    pr5 started

---
+++ pr5_1.js
@@ -0,0 +1,26 @@
+const event = require('events');
+const e = new event.EventEmitter();
+e.on('clicked',function(){
+    const date = new Date()
+    var hour = date.getHours();
+    var min = date.getMinutes();
+    console.log("Today Date is: "+date.getDate()+" / "+(parseInt(date.getMonth())+1)+" / "+date.getFullYear());
+    console.log("Current Time: "+hour+":"+min+":"+date.getSeconds());
+    if(hour>=6 && hour<=11 && min>=0 && min<=59)
+    {
+        console.log("Good Morning");
+    }
+    else if(hour>=12 && hour<=15 && min>=0 && min<=59)
+    {
+        console.log("Good Afternoon");
+    }
+    else if(hour>=16 && hour<=19 && min>=0 && min<=59)
+    {
+        console.log("Good Evening");
+    }
+    else
+    {
+        console.log("Good Night");
+    }
+});
+e.emit('clicked');
