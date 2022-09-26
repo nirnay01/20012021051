eb4310813c7b9130410d1a35635b233dc05d5424
nirnay01 <ravalnirnay@gmail.com>
2022-09-26 10:23:08 +0530

    ok

--- pr5_3.js
+++ pr5_3.js
@@ -4,6 +4,25 @@
 // c. Find out number of words
 // d. Display numbers of words and finishing message
 
-var events = require('events');
-var eventEmitter = new events.EventEmitter();
+const event = require('events');
+const fs = require('fs');
+const e = new event.EventEmitter();
 
+e.on('event-read',(fname)=>{
+    fs.readFile(fname,function(err,data){
+        if(err)
+        {
+            console.log(err.message);
+        }
+        else{
+            console.log(data.toString());
+            e.emit('event-word-cal',data);
+        }
+    });
+});
+e.on('event-word-cal',(data)=>{
+    var str = data.toString().split(' ');
+    console.log("No of word in file is: "+(str.length));
+    console.log("Finish");
+});
+e.emit('event-read','./pr5_3.txt');
