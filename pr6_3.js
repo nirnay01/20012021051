5d9b9365d03bcc9f03b23b1e3fead43e87c86f63
nirnay01 <ravalnirnay@gmail.com>
2022-10-03 10:46:45 +0530

    ok

--- pr6_3.js
+++ pr6_3.js
@@ -1,5 +1,6 @@
 var mysql = require('mysql');
-
+var express=require('express')
+var app=express()
 var con = mysql.createConnection({
   host: "localhost",
   user: "root",
@@ -7,3 +8,5 @@ var con = mysql.createConnection({
   database: "node_test"
 });
 // var sql = "INSERT INTO prodct(id,name,brand,quantity,price) VALUES ()";
+var a='<html><body><form ><table border="1"><tr><td>id</td><td><input type="number" name="id" placeholder="enter product id"></td></tr><tr><td>name</td><td><input type="text" name="name" id="name" placeholder="enter product name"></td></tr><tr><td>brand</td><td><input type="text" name="brand" id="brand" placeholder="enter product brand"></td></tr><tr><td>quantity</td><td><input type="number" name="quantity" id="quantity" placeholder="enter the quantity"></td></tr><tr><td>price</td><td><input type="number" name="price" id="price" placeholder="enter price of product"></td></tr></table></form></body></html>'
+app.use("/",a)
