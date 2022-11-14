65325b4da3e9d4bfb3deb65031ea8573ec855e28
nirnay01 <ravalnirnay@gmail.com>
2022-11-14 10:00:27 +0530

    pr 5 completed

--- pr5_4.js
+++ pr5_4.js
@@ -1,14 +1,8 @@
 // Do as directed:
-// a. Remove a specific listener.
-// b. Remove all listeners.
-// c. How to get the maximum size of listener that will bind with a specific event
-// d. How to change the maximum size of listeners to bind with specific events
-// e. Find out the name of active listeners for the specified event.
-// f. Find out the number of listeners listening to the specified event.
-// g. How to change the execution position of listeners
 const event = require('events');
 const e = new event.EventEmitter();
-
+// a. Remove a specific listener.
+// b. Remove all listeners.
 e.on('event',(err,data)=>{
         if(err)
         {
@@ -28,3 +22,40 @@ e.removeListener('event',(err,data)=>{
     }});
 e.emit('event');
 e.removeAllListeners()
+e.emit('event');
+// c. How to get the maximum size of listener that will bind with a specific event
+// d. How to change the maximum size of listeners to bind with specific events
+e.setMaxListeners(5);
+e.emit('event');
+for(i=0;i<5;i++)
+{
+e.on('event',(err,data)=>{
+    if(err)
+    {
+        console.log(err.message);
+    }
+    else{
+        console.log("Event is added");
+    }
+}
+);
+}
+// e. Find out the name of active listeners for the specified event.
+e.emit('event');
+e.eventNames();
+// f. Find out the number of listeners listening to the specified event.
+e.emit('event');
+e.listenerCount('event');
+// g. How to change the execution position of listeners
+e.emit('event');
+e.prependListener('event',(err,data)=>{
+    if(err)
+    {
+        console.log(err.message);
+    }
+    else{
+        console.log("Event is added");
+    }
+}
+);
+e.emit('event');
